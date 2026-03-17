991a6cdddff36b30d053dbab91e3b9f5b9c78f6c
Liman <77584576+LimanGit@users.noreply.github.com>
2026-03-17 11:41:10 +0600

    Refactor barbor.sh to use ROOTFS_DIR for paths

--- barbor.sh
+++ barbor.sh
@@ -22,27 +22,20 @@ if [ ! -e $ROOTFS_DIR/.installed ]; then
     rm -f $ROOTFS_DIR/rootfs.tar.xz
 
     curl -L --retry 3 \
-        -o /tmp/proot \
+        -o $ROOTFS_DIR/proot \
         "https://github.com/proot-me/proot/releases/download/v${PROOT_VERSION}/proot-v${PROOT_VERSION}-${ARCH}-static"
-    chmod 755 /tmp/proot
-    cp /tmp/proot $ROOTFS_DIR/usr/local/bin/proot
+    chmod 755 $ROOTFS_DIR/proot
 
     curl -L --retry 3 \
-        -o /tmp/gotty.tar.gz \
+        -o $ROOTFS_DIR/gotty.tar.gz \
         "https://github.com/sorenisanerd/gotty/releases/download/v1.5.0/gotty_v1.5.0_linux_${ARCH_ALT}.tar.gz"
-    tar -xzf /tmp/gotty.tar.gz -C $ROOTFS_DIR/usr/local/bin
+    tar -xzf $ROOTFS_DIR/gotty.tar.gz -C $ROOTFS_DIR/usr/local/bin
     chmod 755 $ROOTFS_DIR/usr/local/bin/gotty
+    rm -f $ROOTFS_DIR/gotty.tar.gz
 
-    printf "nameserver 1.1.1.1\nnameserver 1.0.0.1\n" > ${ROOTFS_DIR}/etc/resolv.conf
-
-    rm -f /tmp/gotty.tar.gz
+    printf "nameserver 1.1.1.1\nnameserver 1.0.0.1\n" > $ROOTFS_DIR/etc/resolv.conf
 
     touch $ROOTFS_DIR/.installed
-fi
-
-PROOT_BIN=/tmp/proot
-if [ ! -f "$PROOT_BIN" ]; then
-    PROOT_BIN=$ROOTFS_DIR/usr/local/bin/proot
 fi
 
 printf "\nWelcome to Void Linux (proot)!\n\n"
@@ -52,7 +45,7 @@ printf "   xbps-install -Su         : sync and upgrade all packages\n"
 printf "   xbps-query -Rs [keyword] : search for a package\n"
 printf "   gotty -p [port] -w bash  : share your terminal\n\n"
 
-$PROOT_BIN \
+$ROOTFS_DIR/proot \
 --rootfs="${ROOTFS_DIR}" \
 --link2symlink \
 --kill-on-exit \
@@ -60,6 +53,9 @@ $PROOT_BIN \
 --cwd=/root \
 --bind=/proc \
 --bind=/dev \
+--bind=/sys \
+--bind=/tmp \
+/bin/sh--bind=/dev \
 --bind=/sys \
 --bind=/tmp \
 /bin/sh
