c5d934ca6c7d598860ba56e9345cc9d09896468a
Liman <77584576+LimanGit@users.noreply.github.com>
2026-03-17 11:39:04 +0600

    Simplify extraction of rootfs.tar.xz and cleanup

--- barbor.sh
+++ barbor.sh
@@ -18,9 +18,8 @@ if [ ! -e $ROOTFS_DIR/.installed ]; then
     curl -L --retry 3 -o $ROOTFS_DIR/rootfs.tar.xz \
         "https://repo-default.voidlinux.org/live/current/void-x86_64-ROOTFS-20250202.tar.xz"
 
-    xz -d $ROOTFS_DIR/rootfs.tar.xz
-    tar -xf $ROOTFS_DIR/rootfs.tar -C $ROOTFS_DIR
-    rm -f $ROOTFS_DIR/rootfs.tar
+    tar -xvf $ROOTFS_DIR/rootfs.tar.xz -C $ROOTFS_DIR
+    rm -f $ROOTFS_DIR/rootfs.tar.xz
 
     curl -L --retry 3 \
         -o /tmp/proot \
@@ -33,8 +32,6 @@ if [ ! -e $ROOTFS_DIR/.installed ]; then
         "https://github.com/sorenisanerd/gotty/releases/download/v1.5.0/gotty_v1.5.0_linux_${ARCH_ALT}.tar.gz"
     tar -xzf /tmp/gotty.tar.gz -C $ROOTFS_DIR/usr/local/bin
     chmod 755 $ROOTFS_DIR/usr/local/bin/gotty
-
-    mkdir -p $ROOTFS_DIR/root
 
     printf "nameserver 1.1.1.1\nnameserver 1.0.0.1\n" > ${ROOTFS_DIR}/etc/resolv.conf
 
@@ -59,13 +56,6 @@ $PROOT_BIN \
 --rootfs="${ROOTFS_DIR}" \
 --link2symlink \
 --kill-on-exit \
---root-id \
---cwd=/root \
---bind=/proc \
---bind=/dev \
---bind=/sys \
---bind=/tmp \
-/bin/sh--kill-on-exit \
 --root-id \
 --cwd=/root \
 --bind=/proc \
